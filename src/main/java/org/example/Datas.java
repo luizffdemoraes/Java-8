3249789c6d2b5caca817a50d0d974ad24bcd3769
Luiz Felipe <lffm1994@gmail.com>
2023-02-01 00:41:43 -0300

    A nova API de datas

---
+++ src/main/java/org/example/Datas.java
@@ -0,0 +1,67 @@
+package org.example;
+
+import java.time.*;
+import java.time.format.DateTimeFormatter;
+
+public class Datas {
+
+    public static void main(String[] args) {
+
+        LocalDate hoje = LocalDate.now();
+        System.out.println(hoje);
+
+        LocalDate olimpiadasRio = LocalDate.of(2016, Month.JUNE, 5);
+
+        int anos = olimpiadasRio.getYear() - hoje.getYear();
+
+        System.out.println(anos);
+
+        Period periodo = Period.between(hoje, olimpiadasRio);
+        System.out.println(periodo.getYears());
+        System.out.println(periodo.getMonths());
+        System.out.println(periodo.getDays());
+
+        System.out.println(hoje.minusYears(1));
+        System.out.println(hoje.minusMonths(4));
+        System.out.println(hoje.minusDays(2));
+
+        System.out.println(hoje.plusYears(1));
+        System.out.println(hoje.plusMonths(4));
+        System.out.println(hoje.plusDays(2));
+
+        olimpiadasRio.plusYears(4);
+        System.out.println(olimpiadasRio);
+
+        LocalDate proximasOlimpiadas = olimpiadasRio.plusYears(4);
+        System.out.println(proximasOlimpiadas);
+
+        DateTimeFormatter formatador = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+
+        String valorFormatado = proximasOlimpiadas.format(formatador);
+        System.out.println(valorFormatado);
+
+        DateTimeFormatter formatadorComHoras = DateTimeFormatter.ofPattern("dd/MM/yyyy hh:mm:ss");
+        LocalDateTime agora = LocalDateTime.now();
+        System.out.println(agora.format(formatadorComHoras));
+
+        agora.toLocalDate();
+
+        YearMonth anoEMes = YearMonth.of(2015, Month.JANUARY);
+
+        LocalTime intervalo = LocalTime.of(12, 30);
+        System.out.println(intervalo);
+
+        LocalDate agora1 = LocalDate.now();
+        LocalDate dataFutura = LocalDate.of(2099, Month.JANUARY, 25);
+        System.out.println(agora1);
+        System.out.println(dataFutura);
+
+        Period periodo1 = Period.between(agora1, dataFutura);
+        System.out.println(periodo1);
+
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+        String dataFormatada = formatter.format(agora);
+
+        System.out.println(dataFormatada);
+    }
+}
